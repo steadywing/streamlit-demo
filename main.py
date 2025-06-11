e00da9e250ba498d46c32605b6c7dfa98ec9ef95
steadywing <steadywing01@gmail.com>
2025-06-11 23:56:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
 import streamlit as st
-import json 
 
-st.set_page_config(page_title="뉴콘텐츠아카데미", layout="centered")
-
+st.set_page_config(page_title="뉴콘텐츠아카데미", layout="wide")
 
 pages = [
-    st.Page("pages/components.py", title="Basic", icon=":material/code:", default=True),
-    st.Page("pages/copywriterBot.py", title="CopywriterBot", icon=":material/lightbulb_2:"),
-    st.Page("pages/chatbot.py", title="Chatbot", icon=":material/pets:"),
-    st.Page("pages/chatbot_npc.py", title="Chatbot NPC", icon=":material/robot:")
+    st.Page(
+        page="pages/components.py", 
+        title="Basic", 
+        icon=":material/code:", 
+        default=True
+    ),
+    st.Page(
+        page="pages/02_copywriterBot.py", 
+        title="CopywriterBot", 
+        icon=":material/lightbulb_2:"
+    ),
+    st.Page(
+        page="pages/03_chatbot.py", 
+        title="Chatbot", 
+        icon=":material/pets:"
+    ),
+    st.Page(
+        page="pages/03_chatbot_history.py", 
+        title="Chatbot(대화 저장 기능 추가)", 
+        icon=":material/pets:"
+    ),
+    st.Page(
+        page="pages/04_chatbot_npc.py", 
+        title="Chatbot NPC", 
+        icon=":material/robot:"
+    )
 ]
 
 nav = st.navigation(pages)
