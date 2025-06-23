a1c5bcd22586949f7d987e8049915f55a5647273
steadywing <steadywing01@gmail.com>
2025-06-23 23:36:49 +0900

    Update 06_youtubeChatBot.py

--- pages/06_youtubeChatBot.py
+++ pages/06_youtubeChatBot.py
@@ -33,7 +33,7 @@ ydl_opts = {
     'extract_flat': False,      # 영상 정보만 추출
     'geo_bypass': True,
     'http_headers': {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
         'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
         'Accept': '*/*',
     }
